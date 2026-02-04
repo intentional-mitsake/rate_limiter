9f5615d471472710c227d6bd2cb7d764dbfb6178
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2026-02-04 18:39:20 +0545

    limiter struct defined

--- pkg/limiter/limiter.go
+++ pkg/limiter/limiter.go
@@ -1,1 +1,21 @@
 package limiter
+
+import (
+	"time"
+)
+
+type Bucket struct {
+	capacity         float64   //max tokens
+	refill_rate      float64   //tokens refilled per second
+	tokens           float64   //current amount of tokens
+	last_refill_time time.Time //last time token was added
+}
+
+func CreateBucket(capacity float64, refill_rate float64) *Bucket {
+	return &Bucket{
+		capacity:         capacity,
+		tokens:           capacity, //tho we start with the max capacity, burst prob of sliding window is avoided due to the refil rate
+		refill_rate:      refill_rate,
+		last_refill_time: time.Now(),
+	}
+}
