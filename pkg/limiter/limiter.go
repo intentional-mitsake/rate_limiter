a11eb2e2332f10fb940a66b7e76669fc394b54ba
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2026-02-04 18:57:43 +0545

    limiter func logic

--- pkg/limiter/limiter.go
+++ pkg/limiter/limiter.go
@@ -1,14 +1,17 @@
 package limiter
 
 import (
+	"math"
+	"sync"
 	"time"
 )
 
 type Bucket struct {
-	capacity         float64   //max tokens
-	refill_rate      float64   //tokens refilled per second
-	tokens           float64   //current amount of tokens
-	last_refill_time time.Time //last time token was added
+	capacity         float64    //max tokens
+	refill_rate      float64    //tokens refilled per second
+	tokens           float64    //current amount of tokens
+	last_refill_time time.Time  //last time token was added
+	m                sync.Mutex //avoid race conditions
 }
 
 func CreateBucket(capacity float64, refill_rate float64) *Bucket {
@@ -18,4 +21,27 @@ func CreateBucket(capacity float64, refill_rate float64) *Bucket {
 		refill_rate:      refill_rate,
 		last_refill_time: time.Now(),
 	}
+}
+
+func (b *Bucket) ReqLimiter() bool {
+	//inevitably when used in projects that deal with http req/res
+	//go will use go routines; hence race conditon is a risk
+	b.m.Lock()         //no other goroutines can access while another is accceing the bucket
+	defer b.m.Unlock() //unlock onece the access is done
+
+	now := time.Now()
+	ts_last_refill := now.Sub(b.last_refill_time).Seconds()
+	//refill
+	b.tokens = math.Min(
+		//cant exceed max capacity
+		b.capacity,
+		b.tokens+(ts_last_refill*b.refill_rate),
+	)
+	b.last_refill_time = time.Now()
+	//if theres at least one token we allow the request
+	if b.tokens >= 1 {
+		b.tokens -= 1
+		return true
+	}
+	return false
 }
